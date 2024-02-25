721d191b1d63c140a115b3a3d38e8e4f11355f97
D'Sordes <125225427+dsordes37@users.noreply.github.com>
2024-02-24 22:38:58 -0300

    version 1.0

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { animationFrameScheduler } from 'rxjs';
+import quizzquestions from 'src/assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quiz',
@@ -7,18 +9,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  title:string='Heroi ou Vilão.'
-  question:string='Qual super poder você gostaria de ter ?'
-  options:string[]=['invicibilidade', 'raio laser', 'super força', 'invicibilidade', 'raio laser', 'super força']
-  result:string='Você é um heroi !'
-  option:string='você selecionou tananan'
-  finished:boolean=true
+  title:string=''
+  question:string=''
+  options:any[]=[]
+  result:string=''
+  finished:boolean=false
 
+  round:number=0
+  selecteds:string[]=[]
 
 
   constructor() { }
 
   ngOnInit(): void {
+    if(quizzquestions){
+      this.finished=false
+
+      this.title=quizzquestions.title
+
+      this.question=quizzquestions.questions[this.round].question
+      this.options=quizzquestions.questions[this.round].options
+
+
+
+    }
+  }
+  async checkResult(array:string[]){
+    const result = array.reduce((previous, current, i, array)=>{
+      if(
+        array.filter(item=>item==previous).length>
+        array.filter(item=>item==current).length
+      ){
+        return previous
+      }else{
+        return current
+      }
+    })
+
+    return result
   }
 
+  async proxima(alias:string){
+    this.selecteds.push(alias)
+
+    if(this.round<(quizzquestions.questions.length-1)){
+      
+      this.round++
+
+      this.question=quizzquestions.questions[this.round].question
+      this.options=quizzquestions.questions[this.round].options
+    }else{
+
+      const finalResult:string= await this.checkResult(this.selecteds)
+
+      
+      if(finalResult=='A'){
+        this.result=quizzquestions.results.A
+      }else{
+        this.result=quizzquestions.results.B
+      }
+      this.finished=true
+    } 
+  }
+
+
+  restart(){
+    this.result=''
+    this.selecteds=[]
+    this.round=0
+    this.question=quizzquestions.questions[this.round].question
+    this.options=quizzquestions.questions[this.round].options
+    this.finished=false
+  }
 }
